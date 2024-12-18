1410e9dcda4f31226b63f68a63682e4978ecdf25
Naveen14321432 <naveenkammili04@gmail.com>
2024-12-18 21:04:49 +0530

    Added Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+//import React from 'react'
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h2>Naveen Kammili</h2>
+      <ul className='nav-menu'>
+        <li>Home</li>
+        <li>About me</li>
+        <li>Experience</li>
+        <li>Projects</li>
+        <li>Certifications</li>
+      </ul>
+      <div className="nav-connect">Contact With Me</div>
+    </div>
+  )
+}
+
+export default Navbar
