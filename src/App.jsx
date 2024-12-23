b80899a1c94b2679da7f180ac032b5799590e60a
Naveen14321432 <naveenkammili04@gmail.com>
2024-12-23 21:27:31 +0530

    Added Experience section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import About from './components/about/About'
 import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
+import Services from './components/services/Services'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Services />
     </div>
   )
 }
--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,33 +1,13 @@
 const Services_Data = [
     {
         s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Android App Development Intern(Virtual)",
+        s_desc:"Successfully completed an Android App Development virtual internship under the guidance of Scholverse Educare Private Limited. Gained hands-on experience in building Android applications, learning key concepts like UI/UX design, application logic, and deployment. Enhanced technical proficiency and problem-solving skills through project-based learning."
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Cloud Virtual Internship",
+        s_desc:"Successfully completed a virtual internship focused on cloud technologies, gaining practical experience in deploying, managing, and optimizing cloud-based solutions. Acquired knowledge in key cloud concepts and honed technical skills through hands-on projects."
     },
 ]
  
