5db8621411b4a28d5fea58d4c9d2b66cccceaab6
Naveen14321432 <naveenkammili04@gmail.com>
2024-12-27 16:23:04 +0530

    Changed css

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
       <img src={profile_img} alt='Profile' />
-      <h1><span>I am K. V. S. Naveen,</span> an undergraduate student at KL University.</h1>
+      <h1><span>I am K.V.S.Naveen,</span> an undergraduate student at KL University.</h1>
       <p>I am an undergraduate passionate about leveraging technology to solve real-world problems. I am eager to learn and grow in the fields of software development and engineering, while contributing to innovative projects.</p>
       <div className="hero-action">
         <div className="hero-connect">
